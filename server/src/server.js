9149109377c4b8f07b88430c977a56dcb8996410
dbitton1978-afk <dbitton1978@gmail.com>
2026-04-05 22:15:59 +0300

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { initDB } from "./config/initDB.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({ message: "Music AI Server Running" });
